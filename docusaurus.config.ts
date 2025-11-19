808e7ea99252c8f40707bcd61152982bf14ea830
Chris Jacques <cmjacques@gmail.com>
2025-11-19 11:50:35 +1100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,10 +18,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://npipeline.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/npipeline.dev/',
+  url: 'https://npipeline.dev',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
