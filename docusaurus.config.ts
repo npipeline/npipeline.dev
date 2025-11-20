14595cf4fe1a44ab2b29aea65ce272b560171ca3
Chris Jacques <cmjacques@gmail.com>
2025-11-20 21:34:58 +1100

    Update branding assets and navbar logo
    Replaced references to old logo and branding images in docusaurus.config.ts and removed unused Docusaurus images from static/img. Updated favicon.ico and switched navbar logo to npipeline-logo.svg for improved project branding.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,12 +75,12 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    //image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'NPipeline',
       logo: {
         alt: 'NPipeline logo',
-        src: 'img/logo.svg',
+        src: 'img/npipeline-logo.svg',
       },
       items: [
         {
